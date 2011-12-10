df62ec47531e1655953395a1046aaebc0aefebf5
Rainer Bieniek <Rainer.Bieniek@web.de>
2011-12-10 00:14:57 +0000

    added application skeletons
    git-svn-id: http://www.urb.de/svn/quagga-java/trunk@188 89c0c959-9dd7-408c-8d32-d5a5e101d773

---
+++ java/apps/src/main/java/de/urb/quagga/apps/dumper/PacketDumper.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package de.urb.quagga.apps.dumper;
+
+import de.urb.quagga.weld.SeApplicationBootstrap;
+
+/**
+ * @author rainer
+ *
+ */
+public class PacketDumper {
+
+	public static void main(String[] args) {
+		SeApplicationBootstrap.bootstrapApplication(args);
+	}
+}
--- java/apps/src/main/java/de/urb/quagga/apps/shared/CommandLineConfigurationProducer.java
+++ java/apps/src/main/java/de/urb/quagga/apps/shared/CommandLineConfigurationProducer.java
@@ -1,11 +1,14 @@
 /**
  * 
  */
-package de.urb.quagga.weld;
+package de.urb.quagga.apps.shared;
 
 import javax.inject.Inject;
 
 import org.jboss.weld.environment.se.bindings.Parameters;
+
+import de.urb.quagga.weld.Configuration;
+import de.urb.quagga.weld.ConfigurationProducer;
 
 /**
  * @author rainer
