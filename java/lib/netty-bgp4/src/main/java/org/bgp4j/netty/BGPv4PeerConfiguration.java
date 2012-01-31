8b83d4224ade2694a21f6ab8fc296a24aabf1ace
Rainer Bieniek <Rainer.Bieniek@web.de>
2012-01-31 17:11:32 +0000

    changed file headers to contain Apache 2.0 license header
    git-svn-id: http://www.urb.de/svn/quagga-java/trunk@226 89c0c959-9dd7-408c-8d32-d5a5e101d773

--- java/lib/netty-bgp4/src/main/java/org/bgp4j/netty/BGPv4PeerConfiguration.java
+++ java/lib/netty-bgp4/src/main/java/org/bgp4j/netty/BGPv4PeerConfiguration.java
@@ -1,3 +1,19 @@
+/**
+ *  Copyright 2012 Rainer Bieniek (Rainer.Bieniek@web.de)
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  
+ */
 package org.bgp4j.netty;
 
 import java.net.InetSocketAddress;
@@ -7,7 +23,7 @@ import java.net.InetSocketAddress;
  *
  * The peer configuration <b>MUST</b> implement <code>equals</code> properly.
  * 
- * @author rainer
+ * @author Rainer Bieniek (Rainer.Bieniek@web.de)
  *
  */
 public interface BGPv4PeerConfiguration {
--- java/lib/netty-bgp4/src/main/java/org/bgp4j/netty/service/ValidateServerIdentifier.java
+++ java/lib/netty-bgp4/src/main/java/org/bgp4j/netty/service/ValidateServerIdentifier.java
@@ -1,5 +1,18 @@
 /**
- * 
+ *  Copyright 2012 Rainer Bieniek (Rainer.Bieniek@web.de)
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  
  */
 package org.bgp4j.netty.service;
 
@@ -18,7 +31,7 @@ import org.slf4j.Logger;
  * and the received BGP identifier or the configured and the received autonomous system do not match, the connection is closed.
  * The peer identifier verification is moved out of the finite state machine to make it generic for both client and server case.
  * 
- * @author rainer
+ * @author Rainer Bieniek (Rainer.Bieniek@web.de)
  *
  */
 public class ValidateServerIdentifier extends SimpleChannelUpstreamHandler {
